f05d99f2472c569b909063c63e70db69c8dfd48a
AcYut <kaustubh@acyut.com>
2013-11-06 22:24:52 +0530

    Initial Commit

---
+++ Source/behavior/main.cpp
@@ -0,0 +1,102 @@
+#include <cstdio>
+#include "WorldState.h"
+#include "Tools.h"
+#include "MyBasicBehaviors.h"
+#include "../../Xabsl/XabslEngine/XabslEngine.h"
+#include "../common/common.h"
+#include "../common/commondefs.h"
+
+using namespace std;
+
+playerstate p;
+MyErrorHandler myErrorHandler;
+xabsl::Engine* engine = new xabsl::Engine(myErrorHandler,&getCurrentSystemTime);
+
+BasicBehaviorInitialize basicBehaviorInitialize(myErrorHandler,p);
+BasicBehaviorPrint basicBehaviorPrint(myErrorHandler,p);
+BasicBehaviorUpdate basicBehaviorUpdate(myErrorHandler,p);
+BasicBehaviormoveAcYuttemp basicBehaviormoveAcYuttemp(myErrorHandler,p);
+BasicBehaviorLocalize basicBehaviorLocalize(myErrorHandler,p);
+BasicBehaviorPathToWalk basicBehaviorPathToWalk(myErrorHandler,p);
+BasicBehaviorMakePath basicBehaviorMakePath(myErrorHandler,p);
+BasicBehaviorFindBall basicBehaviorFindBall(myErrorHandler,p);
+
+
+
+void registerXABSL()
+{
+	
+
+	engine->registerEnumeratedInputSymbol("ballreturn", "BallReturns", (int*)&p.ballreturn);
+	engine->registerEnumElement("BallReturns", "BallReturns.BALLFOUND",BALLFOUND);
+	engine->registerEnumElement("BallReturns", "BallReturns.BALLFINDING",BALLFINDING);
+	engine->registerEnumElement("BallReturns", "BallReturns.TURNRIGHT",TURNRIGHT);
+	engine->registerEnumElement("BallReturns", "BallReturns.TURNLEFT",TURNLEFT);
+
+
+	engine->registerEnumeratedInputSymbol("pathreturn", "PathReturns", (int*)&p.pathreturn);
+	engine->registerEnumElement("PathReturns", "PathReturns.DOWALK",DOWALK);
+	engine->registerEnumElement("PathReturns", "PathReturns.DOKICK",DOKICK);
+	engine->registerEnumElement("PathReturns", "PathReturns.DOENCIRCLE",DOENCIRCLE);
+	engine->registerEnumElement("PathReturns", "PathReturns.NOPATH",NOPATH);
+
+	// engine->registerEnumeratedInputSymbol("init.state", "init.state"nt*)&p.initstate);
+	// engine->registerEnumElement("init.state", "init.state.DONE",DONE);
+	// engine->registerEnumElement("init.state", "init.state.NOTDONE",NOTDONE);
+
+
+	engine->registerDecimalInputSymbol("theta", &playerstate::getTheta);
+	engine->registerDecimalInputSymbol("confidence", &playerstate::getConfidence);
+	engine->registerDecimalInputSymbol("resetflag", &playerstate::getReset);
+	engine->registerDecimalInputSymbol("RoboCup.state",&playerstate::getRoboCupState);
+	engine->registerDecimalInputSymbol("ball.distance",&playerstate::getDistance);
+
+
+	engine->registerBasicBehavior(basicBehaviorInitialize);
+	engine->registerBasicBehavior(basicBehaviorLocalize);
+	engine->registerBasicBehavior(basicBehaviorPathToWalk);
+	engine->registerBasicBehavior(basicBehaviorPrint);
+	engine->registerBasicBehavior(basicBehaviorUpdate);
+	engine->registerBasicBehavior(basicBehaviormoveAcYuttemp);
+	engine->registerBasicBehavior(basicBehaviorMakePath);
+	engine->registerBasicBehavior(basicBehaviorFindBall);
+	
+
+
+
+	MyFileInputSource input("intermediate-code.dat");
+    engine->createOptionGraph(input);
+    
+}
+
+void start()
+	{
+	p.resetflag=1; /*Resets all variables in the first run */
+	printf("start(); \n");
+
+	while(1)
+		{
+				printf("start();");
+			/*Get GC data from GC thread */
+			// pthread_mutex_lock(&mutex_GCData);
+   //          p.localgcdata=GCData;
+   //          pthread_mutex_unlock(&mutex_GCData);
+   //          p.GC.update(p.localgcdata);
+	//		printf("Going to execute engine ");		
+			p.localgcdata.state=STATE_PLAYING;
+			engine->execute();
+	//		printf("Executed");
+			p.resetflag=0;
+		}
+	
+	}
+int getImuAngle()
+{
+		fstream f1;
+		f1.open("cpp-src/xsens/imuyaw.angle", ios::in);
+		int value;
+		f1>>value;
+		printf("Value is %d\n", value);
+		f1.close();
+		return value;
+}
---
+++ makefile
@@ -0,0 +1,27 @@
+all: run
+
+run: Source/Build/path.a Source/Build/walk.a Source/Build/behavior.a Source/Build/imgProc.a Source/Build/imu.a run.cpp
+	@echo compiling and linking final code
+	g++ -w run.cpp Source/Build/path.o Source/Build/walk.a Source/Build/behavior.a Source/Build/imgProc.a Source/Build/imu.a -o run -I/usr/include/flycapture -lflycapture -lftdi -lopencv_highgui -lpthread -fpermissive -lopencv_core -lopencv_imgproc  -lcvblob -ltbb -O3 -fpermissive
+
+Source/Build/path.a: Source/path/*.cpp Source/path/*.hpp
+	@make -s -C Source/path
+Source/Build/imu.a: Source/xsens/*.cpp Source/xsens/*.h
+	@make -s -C Source/xsens
+Source/Build/walk.a: Source/walk/*.cpp Source/walk/*.h
+	@make -s -C Source/walk
+Source/Build/behavior.a: Source/behavior/*.cpp Source/behavior/*.h
+	@make -s -C Source/behavior
+Source/Build/imgProc.a: Source/imgProc/*.cpp Source/imgProc/*.h
+	@make -s -C Source/imgProc
+
+
+DOC:
+	@make -s -C Source/behavior/xabsl-src DOC
+clean:
+	@rm -f intermediate-code.dat
+	@rm -f debug-symbols.dat 
+	@rm -f run
+	@make -s -C Source clean
+	@make -s -C Xabsl/compiler clean
+	
