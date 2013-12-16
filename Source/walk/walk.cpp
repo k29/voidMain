658edd2d924f88687970c59aa3b98ae6c52bf229
vaditya04 <vaditya04@yahoo.com>
2013-12-16 22:30:53 +0530

    nmutell

--- Source/walk/walk.cpp
+++ Source/walk/walk.cpp
@@ -671,6 +671,7 @@ int Walk::dribble(double dy, double dx, double t1, double t2)
 	double c = -veloYin;
 	double d = -sspYin;
 	//WALKMOD CODE
+	printf("abcd = %f\t%f\t%f\t%f\n " , a , b , c , d);
 	double A[6] ;
 	A[5] = -sspYin ;
 	A[4] = -veloYin ;
@@ -679,8 +680,8 @@ int Walk::dribble(double dy, double dx, double t1, double t2)
 	A[1] = (15*A[5] - 15*(-sspYfi) + 8*A[4]*sspTime + 7*sspTime*(-veloYfi))/pow(sspTime,4);
 	A[0] = -3*(2*A[5] - 2*(-sspYfi) + sspTime*(A[4]+ (veloYfi)))/pow(sspTime,5);
 
-	printf("abcde = %f\t%f\t%f\t%f\t%f\t%f\n" , A[0] , A[1] , A[2] , A[3] , A[4] , A[5] );
-
+	printf("abcdef = %f\t%f\t%f\t%f\t%f\t%f\n" , A[0] , A[1] , A[2] , A[3] , A[4] , A[5] );
+	printf("sspTime = %f\n" , sspTime);
 	//TILL HERE
 	double height = 390;
 	//double lift   = 30;
