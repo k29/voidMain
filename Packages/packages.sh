6bfa09196be246bebaf3035af67ca2b223994a46
Pratyush Kar <pratyush.kar@gmail.com>
2014-12-15 18:26:09 +0530

    updated packages.sh

--- Packages/packages.sh
+++ Packages/packages.sh
@@ -1,4 +1,3 @@
-# apt-get update
-# apt-get upgrade
-apt-get install synaptic git nautilus-open-terminal xsltproc graphviz ruby1.8 g++ libopencv-dev libcv-dev libhighgui-dev libftdipp1 libftdi-dev cmake libncurses5-dev libtbb-dev libflycapture2-dev 
-
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install synaptic git nautilus-open-terminal xsltproc graphviz build-essential libftdipp1 libftdi-dev cmake libncurses5-dev libtbb-dev libflycapture2-dev curl
